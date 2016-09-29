cb05304cfb85ae699150c5f00a3fd02a648dc04a
Leon <xiaoyankeji@hotmail.com>
2016-09-29 18:07:33 +0800

    第一次提交

---
+++ Log日志记录演示/Form1.vb
@@ -0,0 +1,27 @@
+﻿Public Class Form1
+    Dim SystemLogListener As Logging.FileLogTraceListener = New Logging.FileLogTraceListener
+    Dim SystemTime As Date = My.Computer.Clock.LocalTime
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        With SystemLogListener
+            .Location = Logging.LogFileLocation.ExecutableDirectory
+            .Append = False
+            .AutoFlush = True
+            .Delimiter = vbCrLf
+            .BaseFileName = "HackSystem-" & SystemTime.ToString("yy-MM-dd-hh-mm-ss")
+            .DiskSpaceExhaustedBehavior = Logging.DiskSpaceExhaustedOption.DiscardMessages
+            .Encoding = System.Text.Encoding.UTF8
+            .IndentSize = 4
+            .IndentLevel = 1
+
+            .WriteLine(.BaseFileName & ".log")
+            .WriteLine(My.Computer.Clock.LocalTime.ToString)
+            .WriteLine("————————————————")
+            .Fail("我不爽了，我出错")
+            .Name = "HackSystemLogListener"
+
+            .Flush()
+            .Close()
+        End With
+    End Sub
+End Class
